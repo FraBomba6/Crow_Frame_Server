804e698a2543d73a58100845dc9941f600ef26c9
bomb697 <50354770+bomb697@users.noreply.github.com>
2021-09-02 17:07:41 +0200

    Commit for testing

--- src/server.js
+++ src/server.js
@@ -59,8 +59,10 @@ if (cluster.isMaster) {
             reqNum = {"total": 0}
             counter = 1
             totalPreviousRequests = 0
+            res.socket.end()
         } else if (req.query['stop'] && counting != null) {
             clearInterval(counting)
+            res.socket.end()
         } else {
             res.status(200)
             res.json(reqNum)
